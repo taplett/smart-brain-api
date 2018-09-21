e064273d8122480dfab52373ab5ca62198d6054f
Paul Taplett <taplett@gmail.com>
2018-09-21 06:07:13 -0500

    first commit

---
+++ controllers/profile.js
@@ -0,0 +1,17 @@
+const handleProfileGet = (req, res, db) => {
+    const { id } = req.params;
+    db.select('*').from('users').where({id})
+        .then(user => {
+            if (user.length) {
+                res.json(user[0])
+            } else {
+                res.status(400).json('Not Found')
+            }
+            res.json(user[0])
+    })
+        .catch(err => res.status(400).json('Error Getting User'))
+}
+
+module.exports = {
+    handleProfileGet
+}
